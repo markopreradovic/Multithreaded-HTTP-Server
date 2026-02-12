5226e0c4f4bd310d8ad70597a85fcfc5c1f9d79d
marko <marko.preradovic416@gmail.com>
2026-02-12 14:37:36 +0100

    Basic accept loop + epoll.

--- src/main.c
+++ src/main.c
@@ -10,9 +10,11 @@
 #include <sys/socket.h>  // socket, bind, listen...
 #include <netinet/in.h>  // sockaddr_in, htons...
 #include <arpa/inet.h>   // inet_ntoa (for debugging later)
+#include <sys/epoll.h>
 
 #define DEFAULT_PORT 8080
 #define BACKLOG 128
+#define MAX_EVENTS 64
 #define SERVER_NAME "CServer/0.1"
 
 volatile sig_atomic_t running = 1;
@@ -64,6 +66,7 @@ int main(void) {
     //Handlers.
     signal(SIGINT, signal_handler); //Ctrl C
     signal(SIGTERM, signal_handler);
+    signal(SIGPIPE, SIG_IGN); //useful for writing on an closed socket
 
     //Socket binding.
     int server_fd = create_and_bind_socket(DEFAULT_PORT);
@@ -72,21 +75,64 @@ int main(void) {
         return EXIT_FAILURE;
     }
 
+    //EPOLL init
+    int epoll_fd = epoll_create1(0);
+    if (epoll_fd < 0) {
+        perror("epoll_create1 failed");
+        close(server_fd);
+        return EXIT_FAILURE;
+    }
+
+    //adding server socket to epoll
+    struct epoll_event ev = {0};
+    ev.events = EPOLLIN; //readable = new con
+    ev.data.fd = server_fd;
+
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &ev) < 0) {
+        perror("epoll_ctl ADD server_fd failed");
+        close(epoll_fd);
+        close(server_fd);
+        return EXIT_FAILURE;
+    }
+
+    struct epoll_event events[MAX_EVENTS];
+
     printf("Server running. Ctrl + C to shut down.");
+    fflush(stdout);
     while (running) {
-        printf("Awaiting connections...\n");
-        fflush(stdout);
-        int remaining = 5;           // čekamo 5 sekundi ukupno
-        while (remaining > 0 && running) {
-            sleep(1);
-            remaining--;
+        int nfds = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+        if (nfds<0) {
+            if (errno == EINTR && !running) {
+                break; // signal interrupted wait
+            }
+            perror("epoll_wait failed");
+            continue;
+        }
+
+        for (int i = 0; i < nfds; i++) {
+            if (events[i].data.fd == server_fd) {
+                struct sockaddr_in client_addr;
+                socklen_t client_len = sizeof(client_addr);
+
+                int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
+                if (client_fd < 0) {
+                    perror("accept failed");
+                    continue;
+                }
+
+                char client_ip[INET_ADDRSTRLEN];
+                inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, sizeof(client_ip));
+                printf("New connection from %s:%d  (fd=%d)\n", client_ip, ntohs(client_addr.sin_port), client_fd);
+
+                close(client_fd);
+            }
         }
     }
 
     //Cleanup.
     printf("Shutdown signal received...\n");
+    close(epoll_fd);
     close(server_fd);
     printf("Server closed.\n");
     return EXIT_SUCCESS;
-
 }
